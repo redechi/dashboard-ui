49136fbb61635533c4fd7ae2cc32ddaa8dfc77c3
Sam Merry <sam.merry@gmail.com>
2014-05-30 17:11:06 -0700

    adding reps and cleaning router.

--- scripts/router.js
+++ scripts/router.js
@@ -1,20 +1,17 @@
 define([
   'backbone',
   './controllers/router'
-  ],function (backbone, controller) {
-    'use strict';
+],function (backbone, controller) {
+  'use strict';
 
-    var Router = backbone.Marionette.AppRouter.extend({
+  var Router = backbone.Marionette.AppRouter.extend({
 
-        appRoutes: {
-            'test/:what'      : 'test'
-        },
+    appRoutes: {
+      'test/:what'      : 'test'
+    },
 
-        controller: controller
+    controller: controller
+  });
 
-    });
-
-    var r = new Router();
-    console.log(r);
-    return r;
+  return new Router();
 });
