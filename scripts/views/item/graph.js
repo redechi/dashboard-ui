19eb134282ba51549b1b58e40ad867b359b84b05
Brendan Nee <brendan@blinktag.com>
2014-06-04 15:23:44 -0700

    Graph order

--- scripts/views/item/graph.js
+++ scripts/views/item/graph.js
@@ -16,7 +16,7 @@ function( Backbone, comms, _, trips, GraphTmpl, formatters) {
 
     initialize: function(model) {
       console.log("initialize a Graph ItemView");
-      this.collection.graphType = 'distance_miles';
+      this.collection.graphType = 'average_mpg';
     },
 
     collection: trips, // trips singleton
