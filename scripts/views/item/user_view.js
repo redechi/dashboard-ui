8b20c025c73341602befe55a54854a2622b1fe48
Sam Merry <sam.merry@gmail.com>
2014-06-06 12:44:05 -0700

    fixing tests

--- scripts/views/item/user_view.js
+++ scripts/views/item/user_view.js
@@ -1,41 +1,41 @@
 define([
-	'backbone',
-	'hbs!tmpl/item/user_view_tmpl',
-	'../../models/user'
+  'backbone',
+  'hbs!tmpl/item/user_view_tmpl',
+  '../../models/user'
 ],
 function( Backbone, UserViewTmpl, user  ) {
     'use strict';
 
-	/* Return a ItemView class definition */
-	return Backbone.Marionette.ItemView.extend({
+  /* Return a ItemView class definition */
+  return Backbone.Marionette.ItemView.extend({
 
-		initialize: function() {
-			console.log("initialize a UserView ItemView");
+    initialize: function() {
+      console.log("initialize a UserView ItemView");
 
-			this.model.fetch();
-		},
+      this.model.fetch();
+    },
 
-  	template: UserViewTmpl,
+    template: UserViewTmpl,
 
-  	/* ui selector cache */
-  	ui: {},
+    /* ui selector cache */
+    ui: {},
 
-		/* Ui events hash */
-		events: {
-		},
+    /* Ui events hash */
+    events: {
+    },
 
-		/* on render callback */
-		onRender: function() {
-			setTimeout(function() {
-				$('.btn-user').popover({
-					html: true,
-					content: function() { return $('.userPopoverContent').html(); },
-					placement: 'bottom'
-				});
-			}, 0);
-		},
+    /* on render callback */
+    onRender: function() {
+      setTimeout(function() {
+        $('.btn-user').popover({
+          html: true,
+          content: function() { return $('.userPopoverContent').html(); },
+          placement: 'bottom'
+        });
+      }, 0);
+    },
 
-		model: user
-	});
+    model: user
+  });
 
 });
--- test/SpecRunner.js
+++ test/SpecRunner.js
@@ -1,41 +1,49 @@
 require.config({
-    baseUrl: '/job/DeLorean-Dashboard/Mocha_Tests/test/spec',//
-    'test/../../scripts',
+    baseUrl: '/job/DeLorean-Dashboard/Mocha_Tests/scripts',//'/scripts',
     urlArgs: 'cb=' + Math.random(),
 
     deps: ['backbone.marionette'],
 
     paths: {
-        spec: 'spec', // lives in the test directory
+        spec: '../test/spec', // lives in the test directory
+        amlCollection: 'base/collection',
+        amlAggregate: 'controllers/aggregate',
+        amlSort: 'controllers/sort',
 
-        jquery: '../../bower_components/jquery/jquery',
-        backbone: '../../bower_components/backbone-amd/backbone',
-        underscore: '../../bower_components/underscore-amd/underscore',
+        jquery: '../bower_components/jquery/jquery',
+        backbone: '../bower_components/backbone-amd/backbone',
+        underscore: '../bower_components/underscore-amd/underscore',
+
+        moment: '../bower_components/momentjs/moment',
+        d3: '../bower_components/d3/d3',
+        nvd3: '../bower_components/nvd3/nv.d3',
+        mapbox: '//api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox',
+        markercluster: '//api.tiles.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster',
 
         /* backbone plugins */
-        'backbone.syphon': '../../bower_components/backbone.syphon/lib/amd/backbone.syphon',
-        'backbone.iobind': '../../bower_components/backbone.iobind/dist/backbone.iobind',
+        'backbone.syphon': '../bower_components/backbone.syphon/lib/amd/backbone.syphon',
+        'backbone.iobind': '../bower_components/backbone.iobind/dist/backbone.iobind',
 
         /* alias all marionette libs */
-        'backbone.marionette': '../../bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
-        'backbone.wreqr': '../../bower_components/backbone.wreqr/lib/amd/backbone.wreqr', 
-        'backbone.babysitter': '../../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
+        'backbone.marionette': '../bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
+        'backbone.wreqr': '../bower_components/backbone.wreqr/lib/amd/backbone.wreqr', 
+        'backbone.babysitter': '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
 
         /* alias the bootstrap js lib */
         bootstrap: 'vendor/bootstrap',
         'bootstrap-button': 'vendor/bootstrap-button',
 
         /* Alias text.js for template loading and shortcut the templates dir to tmpl */
-        text: '../../bower_components/requirejs-text/text',
-        tmpl: 'templates',
+        text: '../bower_components/requirejs-text/text',
+        tmpl: '../templates',
 
         /* handlebars from the require handlerbars plugin below */
-        handlebars: '../../bower_components/require-handlebars-plugin/Handlebars',
+        handlebars: '../bower_components/require-handlebars-plugin/Handlebars',
 
         /* require handlebars plugin - Alex Sexton */
-        i18nprecompile: '../../bower_components/require-handlebars-plugin/hbs/i18nprecompile',
-        json2: '../../bower_components/require-handlebars-plugin/hbs/json2',
-        hbs: '../../bower_components/require-handlebars-plugin/hbs'
+        i18nprecompile: '../bower_components/require-handlebars-plugin/hbs/i18nprecompile',
+        json2: '../bower_components/require-handlebars-plugin/hbs/json2',
+        hbs: '../bower_components/require-handlebars-plugin/hbs'
     },
 
     hbs: {
@@ -46,7 +54,7 @@ require.config({
 /* require test suite */
 require([
     'jquery',
-    'testSuite'
+    'spec/testSuite'
 ],
 function( $, testSuite ) {
   'use strict';
