67a2c9a663a327fbe9ac0891b073b1fa55985021
Sam Merry <sam.merry@gmail.com>
2014-06-12 19:01:26 -0700

    redo cookie session

--- scripts/application.js
+++ scripts/application.js
@@ -10,12 +10,8 @@ function( Backbone, Communicator, router, regionManager, tripsCollection ) {
   'use strict';
 
   // simple session storage
-
-  function getCookie(key) {
-    return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
-  }
-
-  var accessToken = getCookie('token') || 'ba56eee32df6be1437768699247b406fc7d9992f';
+  var accessMatch = /[;\s]+?token=([^;]+)/.exec(document.cookie) || [];
+  var accessToken = accessMatch.pop() || 'ba56eee32df6be1437768699247b406fc7d9992f';
 
   $.ajaxSetup({
     headers: {'Authorization': 'token ' + accessToken},
