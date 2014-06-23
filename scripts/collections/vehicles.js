b595d5d4597ef3eda7aee0873e538a79192fd558
Brendan Nee <brendan@blinktag.com>
2014-06-23 03:24:51 -0700

    vehicles collection and model

---
+++ scripts/collections/vehicles.js
@@ -0,0 +1,24 @@
+define([
+  'backbone',
+  'models/vehicle',
+  '../controllers/filter'
+],
+function( Backbone, Vehicle, filterList) {
+  'use strict';
+
+  /* vehicles singleton */
+  var Vehicles = Backbone.AML.Collection.extend({
+
+    model: Vehicle,
+    url: 'https://api.automatic.com/v1/vehicles',
+    comparator: 'display_name',
+
+    initialize: function() {
+      console.log('initialize a Vehicles collection');
+    }
+
+  });
+
+  // make this a singleton
+  return new Vehicles();
+});
