92b1185693e94872d826d005b7b67bb5fe144535
Brendan Nee <brendan@blinktag.com>
2014-06-26 19:25:42 -0700

    make reloading with filters work

--- scripts/controllers/router.js
+++ scripts/controllers/router.js
@@ -9,29 +9,19 @@ define([
 function( Backbone, regionManager, tripsCollection, filtersCollection, SummaryLayout, TripLayout) {
   'use strict';
 
-  var currentView = undefined;
-
   var contentRegion = regionManager.getRegion('main_content');
   window.TripView = TripLayout;
   window.region = contentRegion;
 
   return {
 
-
     showSummaryLayout: function () {
-
       filtersCollection.fromUrl();
-      // var dateFilter = filtersCollection.findWhere({name: 'date'});
-      // if (!dateFilter) filtersCollection.addDateFilter();
-      tripsCollection.applyAllFilters();
 
       var contentRegion = regionManager.getRegion('main_content');
-      if (currentView !== 'summary') {
-        var summary = new SummaryLayout();
-        contentRegion.show(summary);
-      }
-
-      currentView = 'summary';
+      var summary = new SummaryLayout();
+      contentRegion.show(summary);
+      tripsCollection.applyAllFilters();
     },
 
 
@@ -42,7 +32,6 @@ function( Backbone, regionManager, tripsCollection, filtersCollection, SummaryLa
       var trip = new TripLayout({collection: tripCollection});
       var contentRegion = regionManager.getRegion('main_content');
       contentRegion.show(trip);
-      currentView = 'trip_layout';
     },
 
 
@@ -56,7 +45,6 @@ function( Backbone, regionManager, tripsCollection, filtersCollection, SummaryLa
       document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
       window.location = '/';
     }
-
 
   };
 });
