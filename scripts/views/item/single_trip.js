39f6795a79147212cbfa844748f5bde71e8362d7
Brendan Nee <brendan@blinktag.com>
2014-08-07 18:34:35 -0700

    Put the full address on the trip page

--- scripts/views/item/single_trip.js
+++ scripts/views/item/single_trip.js
@@ -1,8 +1,9 @@
 define([
   'backbone',
-  'hbs!tmpl/item/single_trip_tmpl'
+  'hbs!tmpl/item/single_trip_tmpl',
+  '../../controllers/unit_formatters'
 ],
-function( Backbone, SingleTripTmpl ) {
+function( Backbone, SingleTripTmpl, formatters ) {
   'use strict';
 
   return Backbone.Marionette.ItemView.extend({
@@ -16,6 +17,12 @@ function( Backbone, SingleTripTmpl ) {
     templateHelpers: {
       over60Minutes: function() {
         return (this.duration >= 60);
+      },
+      startAddress: function() {
+        return formatters.formatAddress(this.start_location.name);
+      },
+      endAddress: function() {
+        return formatters.formatAddress(this.end_location.name);
       }
     }
   });
