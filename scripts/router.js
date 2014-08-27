c420ef5701fea3aadf38f0e52d42c056ae69cb67
Brendan Nee <brendan@blinktag.com>
2014-08-26 17:56:52 -0700

    Google Analytics Tracking

--- scripts/router.js
+++ scripts/router.js
@@ -7,6 +7,10 @@ define([
 
   var Router = backbone.Marionette.AppRouter.extend({
 
+    initialize: function() {
+      window.addEventListener('hashchange', this.trackPageview, false);
+    },
+
     appRoutes: {
       '(/)': 'showSummaryLayout',
       'login': 'showLoginLayout',
@@ -39,6 +43,40 @@ define([
           callback.apply(router, arguments);
         };
         return Backbone.Router.prototype.route.call(this, route, name, f);
+    },
+
+
+    onRoute: function() {
+      //track every route change as a page view in google analytics
+      this.trackPageview();
+    },
+
+
+    trackPageview: function () {
+      var gaFragment = Backbone.history.getFragment();
+
+      //only track if changed.
+      if(this.gaFragment === gaFragment) {
+        return;
+      }
+
+      this.gaFragment = gaFragment;
+
+      //prepend slash
+      if (!/^\//.test(gaFragment) && gaFragment !== '') {
+        gaFragment = '/' + gaFragment;
+      }
+
+      var ga;
+      if (window.GoogleAnalyticsObject && window.GoogleAnalyticsObject !== 'ga') {
+        ga = window.GoogleAnalyticsObject;
+      } else {
+        ga = window.ga;
+      }
+
+      if (typeof ga !== 'undefined') {
+        ga('send', 'pageview', gaFragment);
+      }
     },
 
 
