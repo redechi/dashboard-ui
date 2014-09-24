1f7478ca1ea01820f21a38238407cc8e44a79ecd
Brendan Nee <brendan@blinktag.com>
2014-09-24 13:13:17 -0700

    Migrate dashboard to new scopes
    Test Plan: tested locally (will need to have apps scopes migrated in order to fully test)
    
    Reviewers: sam, maru
    
    Reviewed By: sam, maru
    
    Subscribers: jenkins
    
    Differential Revision: https://phabricator.automatic.co/D1491

--- scripts/views/layout/login.js
+++ scripts/views/layout/login.js
@@ -82,7 +82,7 @@ function( Backbone, regionManager, LoginTmpl, login, analytics ) {
             grant_type: 'password',
             username: email,
             password: password,
-            scope: 'scope:trip:summary scope:location scope:mil:off scope:ignition:on scope:ignition:off scope:mil:on scope:notification:speeding scope:region:changed scope:notification:hard_brake scope:user:details scope:notification:hard_accel scope:vehicle scope:admin'
+            scope: 'scope:trip scope:location scope:vehicle:profile scope:vehicle:events scope:user:profile scope:automatic'
           },
           function(data) {
             if(data && data.access_token) {
