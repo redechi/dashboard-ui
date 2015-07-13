6af825aefdb13b7c3b2181adc70329368bf3fc46
Brendan Nee <brendan@blinktag.com>
2015-07-13 16:05:33 -0700

    Updates to nvd3 donut
    Summary: Simplifications for newer versions of nvd3
    
    Test Plan: tested locally
    
    Reviewers: yanglin
    
    Reviewed By: yanglin
    
    Subscribers: jenkins
    
    Differential Revision: https://phabricator.automatic.co/D7160

--- scripts/controllers/graph_helpers.js
+++ scripts/controllers/graph_helpers.js
@@ -8,76 +8,72 @@ function( formatters ) {
   return {
     scoreGraph: function(score, container, options) {
 
-      //put nvd3 into production mode
-      nv.dev = false;
+      var scoreColor = formatters.scoreColor(score);
+      var chart = nv.models.pieChart()
+          .x(function(d) { return d.label; })
+          .y(function(d) { return d.value; })
+          .showLabels(false)
+          .labelThreshold(0.05)
+          .labelType('percent')
+          .donut(true)
+          .donutRatio(options.donut)
+          .growOnHover(false)
+          .margin({top:0, right:0, bottom:0, left:0})
+          .showLegend(false)
+          .startAngle(function(d) {
+            return d.startAngle + Math.PI;
+          })
+          .endAngle(function(d) {
+            return d.endAngle + Math.PI;
+          })
+          .color([scoreColor, '#E3E2DE']);
 
-      var scoreColor = formatters.scoreColor(score),
-          trans = 100 - score;
+      container.empty();
 
-      nv.addGraph(function() {
-        var chart = nv.models.pieChart()
-            .x(function(d) { return d.label; })
-            .y(function(d) { return d.value; })
-            .showLabels(false)    //Display pie labels
-            .labelThreshold(0.05)  //Configure the minimum slice size for labels to show up
-            .labelType('percent') //Configure what type of data to show in the label. Can be "key", "value" or "percent"
-            .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
-            .donutRatio(options.donut)      //Configure how big you want the donut hole size to be.
-            .margin({top:0, right:0, bottom:0, left:0})
-            .showLegend(false)
-            .color([scoreColor, '#E3E2DE']);
+      var svg = d3.select(container.get(0));
 
-        container.empty();
+      var datum = [
+        {
+          label: 'score',
+          value: 0
+        },
+        {
+          label: 'Transparency',
+          value: 100
+        }
+      ];
 
-        var svg = d3.select(container.get(0));
+      svg.attr('width', options.width)
+         .attr('height', options.height);
 
-        var datum = [
-          {
-            label: 'score',
-            value: 0
-          },
-          {
-            label: 'Transparency',
-            value: 100
-          }
-        ];
+      svg.datum(datum)
+        .transition().duration(options.duration)
+        .call(chart);
 
-        svg.attr('width', options.width)
-           .attr('height', options.height);
+      // duplicate set for animation.
+      datum = [
+        {
+          label: 'score',
+          value: score
+        },
+        {
+          label: 'Transparency',
+          value: 100 - score
+        }
+      ];
 
-        svg.datum(datum)
-          .transition().duration(options.duration)
-          .call(chart);
+      svg.datum(datum)
+        .transition().duration(options.duration)
+        .call(chart);
 
-        // duplicate set for animation.
-        datum = [
-          {
-            label: 'score',
-            value: score
-          },
-          {
-            label: 'Transparency',
-            value: trans
-          }
-        ];
+      svg.append('text')
+        .attr('x', (options.width / 2))
+        .attr('y', (options.height * 0.65))
+        .attr('text-anchor', 'middle')
+        .attr('fill', scoreColor)
+        .attr('class', 'averageScore')
+        .text(parseInt(datum[0].value));
 
-        svg.datum(datum)
-          .transition().duration(options.duration)
-          .call(chart);
-
-        svg.append('text')
-          .attr('x', (options.width / 2))
-          .attr('y', (options.height * 0.65))
-          .attr('text-anchor', 'middle')
-          .attr('fill', scoreColor)
-          .attr('class', 'averageScore')
-          .text(parseInt(datum[0].value));
-
-        // rotate the pie chart 180
-        d3.select('.nv-pieChart').attr('transform', 'rotate(180,' + (options.width / 2) + ',' + (options.height / 2) + ')');
-
-        return chart;
-      });
     }
   };
 });
