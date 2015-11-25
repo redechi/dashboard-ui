ae21edf9c2fdf24ee7fe2facdd41c4b1d4880020
Brendan Nee <brendan@blinktag.com>
2015-11-25 13:27:03 -0800

    Prep demo trips

--- src/_assets/js/requests.js
+++ src/_assets/js/requests.js
@@ -8,6 +8,7 @@ exports.getTrips = (cb) => {
   if(login.isLoggedIn()) {
     cb();
   } else {
+    // Demo Trips
     request
       .get('/data/trips.json')
       .set('Accept', 'application/json')
@@ -19,7 +20,19 @@ exports.getTrips = (cb) => {
           return cb(new Error('No trip results returned'));
         }
 
-        return cb(null, response.body.results);
+        return cb(null, prepDemoTrips(response.body.results));
       });
   }
 };
+
+function prepDemoTrips(trips) {
+  // Make all demo trips recent
+  let offset = moment().diff(moment(_.first(trips).started_at));
+  let dayOffset = moment.duration(Math.floor(moment.duration(offset).asDays()), 'days');
+
+  return trips.map((trip) => {
+    trip.started_at = moment(trip.started_at).add(dayOffset, 'days').toISOString();
+    trip.ended_at = moment(trip.ended_at).add(dayOffset, 'days').toISOString();
+    return trip;
+  });
+}
