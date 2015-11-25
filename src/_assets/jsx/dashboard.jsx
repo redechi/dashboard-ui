e3b7da803c311f81f5d4cbc1b82ed541da1222ba
Brendan Nee <brendan@blinktag.com>
2015-11-24 17:26:55 -0800

    Move totals calc to dashboard.jsx

--- src/_assets/jsx/dashboard.jsx
+++ src/_assets/jsx/dashboard.jsx
@@ -78,23 +78,53 @@ module.exports = class Dashboard extends React.Component {
     }
   }
 
+  calculateTotals() {
+    if(!this.state.trips) {
+      return false;
+    }
+
+    let stats = this.state.trips.reduce((memo, trip) => {
+      memo.distance += trip.distance_miles;
+      memo.duration += trip.duration_s;
+      memo.fuel += trip.fuel_volume_gal;
+      memo.cost += trip.fuel_cost_usd;
+      memo.sumScoreEvents += trip.scoreEvents * trip.duration_s;
+      memo.sumscoreSpeeding += trip.scoreSpeeding * trip.duration_s;
+      return memo;
+    }, {distance: 0, duration: 0, fuel: 0, cost: 0, sumScoreEvents: 0, sumScoreSpeeding: 0});
+
+    let scoreEvents = (stats.sumScoreEvents / stats.duration) || 0;
+    let scoreSpeeding = (stats.sumScoreSpeeding / stats.duration) || 0;
+    stats.score = Math.max(0, scoreEvents) + Math.max(0, scoreSpeeding);
+
+    stats.mpg = (stats.distance / stats.fuel) || 0;
+
+    return {
+      distance: formatters.distance(stats.distance),
+      duration: formatters.durationHours(stats.duration),
+      score: formatters.score(stats.score),
+      cost: formatters.costWithUnit(stats.cost),
+      mpg: formatters.averageMPG(stats.mpg)
+    };
+  }
+
   areAllSelected() {
     return _.every(this.state.trips, (trip) => trip.selected);
   }
 
   render() {
+    let totals = this.calculateTotals();
+
     return (
       <div>
         <Header />
         <div>
           <Filters />
           <div>
-            <div>
-              <Graph trips={this.state.trips} />
-              <Map trips={this.state.trips} />
-            </div>
             <div className="right-column">
-              <TripStats trips={this.state.trips} />
+              <TripStats
+                trips={this.state.trips}
+                totals={totals} />
               <TripList
                 trips={this.state.trips}
                 allSelected={this.state.allSelected}
@@ -102,6 +132,12 @@ module.exports = class Dashboard extends React.Component {
                 toggleSelectAll={this.toggleSelectAll}
                 export={this.export}
                 exporting={this.state.exporting} />
+            </div>
+            <div className="left-column">
+              <Graph
+                trips={this.state.trips}
+                totals={totals} />
+              <Map trips={this.state.trips} />
             </div>
           </div>
         </div>
--- src/_assets/jsx/trip_stats.jsx
+++ src/_assets/jsx/trip_stats.jsx
@@ -1,39 +1,8 @@
 import React from 'react';
-var formatters = require('../js/formatters');
 
 module.exports = class TripStats extends React.Component {
   constructor(props) {
     super(props);
-  }
-
-  calculateStats() {
-    if(!this.props.trips) {
-      return false;
-    }
-
-    let stats = this.props.trips.reduce((memo, trip) => {
-      memo.distance += trip.distance_miles;
-      memo.duration += trip.duration_s;
-      memo.fuel += trip.fuel_volume_gal;
-      memo.cost += trip.fuel_cost_usd;
-      memo.sumScoreEvents += trip.scoreEvents * trip.duration_s;
-      memo.sumscoreSpeeding += trip.scoreSpeeding * trip.duration_s;
-      return memo;
-    }, {distance: 0, duration: 0, fuel: 0, cost: 0, sumScoreEvents: 0, sumScoreSpeeding: 0});
-
-    let scoreEvents = (stats.sumScoreEvents / stats.duration) || 0;
-    let scoreSpeeding = (stats.sumScoreSpeeding / stats.duration) || 0;
-    stats.score = Math.max(0, scoreEvents) + Math.max(0, scoreSpeeding);
-
-    stats.mpg = (stats.distance / stats.fuel) || 0;
-
-    return {
-      distance: formatters.distance(stats.distance),
-      duration: formatters.durationHours(stats.duration),
-      score: formatters.score(stats.score),
-      cost: formatters.costWithUnit(stats.cost),
-      mpg: formatters.averageMPG(stats.mpg)
-    };
   }
 
   render() {
@@ -46,24 +15,22 @@ module.exports = class TripStats extends React.Component {
         </div>
       );
     } else {
-      var stats = this.calculateStats();
-
       return (
         <div className="trip-stats">
           <div className="stat distance">
-            <div className="value">{stats.distance}</div>
+            <div className="value">{this.props.totals.distance}</div>
             <div className="label">Miles</div>
           </div>
           <div className="stat mpg active">
-            <div className="value">{stats.mpg}</div>
+            <div className="value">{this.props.totals.mpg}</div>
             <div className="label">MPG</div>
           </div>
           <div className="stat cost">
-            <div className="value">{stats.cost}</div>
+            <div className="value">{this.props.totals.cost}</div>
             <div className="label">Fuel</div>
           </div>
           <div className="stat duration">
-            <div className="value">{stats.duration}</div>
+            <div className="value">{this.props.totals.duration}</div>
             <div className="label">Hours</div>
           </div>
         </div>
