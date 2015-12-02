9c4631dd574ac8dba46a3bbbe8a97e012f8badc3
Brendan Nee <brendan@blinktag.com>
2015-12-02 11:53:07 -0800

    noMatch page

--- src/_assets/jsx/nomatch.jsx
+++ src/_assets/jsx/nomatch.jsx
@@ -1,5 +1,21 @@
-module.exports = function(props) {
-  return (
-    <div className="nomatch">Page Not Found</div>
-  );
+import React from 'react';
+import { Link } from 'react-router';
+
+const Header = require('./header.jsx');
+
+
+module.exports = class NoMatch extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <Header />
+        <div className="container no-match">
+          <h1>This page cannot be found.</h1>
+          <div className="big-alert"></div>
+          <p>Oops!  The address you have entered is incorrect or the page no longer exists.</p>
+          <Link to="/" className="btn btn-blue">Go to the Dashboard</Link>
+        </div>
+      </div>
+    );
+  }
 };
