a5b3d831984c46ca408b31a168a83914ee929361
Brendan Nee <brendan@blinktag.com>
2016-01-26 16:43:57 -0800

    Fuel cost dollar sign

--- src/_assets/jsx/trip_list.jsx
+++ src/_assets/jsx/trip_list.jsx
@@ -326,7 +326,7 @@ class TripList extends React.Component {
                   <label>MPG</label>
                 </div>
                 <div className="stat cost">
-                  <div className="value">{formatters.cost(trip.fuel_cost_usd)}</div>
+                  <div className="value">${formatters.cost(trip.fuel_cost_usd)}</div>
                   <label>Fuel</label>
                 </div>
                 <div className={classNames('duration', 'stat', { hours: trip.duration_s >= (60 * 60) })}>
