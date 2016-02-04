af3caeb32a3b07f72376baae6ecb94e58d34e154
Brendan Nee <brendan@blinktag.com>
2016-02-03 17:52:34 -0800

    Add sentry

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import { IS_LOCAL } from './config.js';
 
 require('babel-polyfill');
 
+import Raven from 'raven-js';
+Raven
+    .config('https://1e1818d9c5714d4990a766550ec593db@app.getsentry.com/33040', {
+      whitelistUrls: ['dashboard.automatic.com/']
+    })
+    .install();
+
 require('./_assets/scss/main.scss');
 
 require('./_assets/jsx/app.jsx');
