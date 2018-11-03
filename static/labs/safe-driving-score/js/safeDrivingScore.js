408f782f8306a15124fecf80d0a096f181ac8201
Brendan Nee <brendan@blinktag.com>
2018-11-03 00:16:26 -0700

    SDS connected to Moxie and history graph
    Test Plan: test on staging
    
    Reviewers: arpan
    
    Reviewed By: arpan
    
    Subscribers: jenkins
    
    Differential Revision: https://phabricator.automatic.co/D29377

--- static/labs/safe-driving-score/js/safeDrivingScore.js
+++ static/labs/safe-driving-score/js/safeDrivingScore.js
@@ -1,5 +1,5 @@
 function createDemoDrivingScore(vehicleId) {
-  const random = Math.random()
+  const random = Math.random();
 
   return {
     score: Math.ceil(random * 500) + 400,
@@ -37,7 +37,7 @@ function createDemoDrivingScore(vehicleId) {
           description: 'Amount of time spent at high speeds ( >75 mph) or above the speed limit of the road.',
           coaching_text: 'Slow down.',
           unit: '%',
-          unit_label: 'of driving time',
+          unit_descriptor: 'of driving time',
           value: Math.round(2 * (1.25 - Math.random() * 0.5) * 10) / 10,
           relative_value: Math.round(1.5 * (1.15 - Math.random() * 0.3) * 10) / 10,
           relative_value_label: 'Avg. for other drivers'
@@ -47,7 +47,7 @@ function createDemoDrivingScore(vehicleId) {
           description: 'Average number of hard brakes while driving in urban areas. Hard braking (or hard acceleration)  is a driver event when more force than normal is applied to the vehicle\'s brake or accelerator. It can be an indicator of aggressive or unsafe driving and therefore contributes negatively to the score',
           coaching_text: 'Pay more attention and leave greater distance between your car and the one in front of you.',
           unit: '',
-          unit_label: 'hard brakes every 100km',
+          unit_descriptor: 'hard brakes every 100km',
           value: Math.round(18 * (1.25 - Math.random() * 0.5) * 10) / 10,
           relative_value: Math.round(5 * (1.15 - Math.random() * 0.3) * 10) / 10,
           relative_value_label: 'Avg. for other drivers'
@@ -59,7 +59,7 @@ function createDemoDrivingScore(vehicleId) {
           description: 'Number of trips that are long (3+ hrs) or that were somewhat long (1+ hrs) but did not include long enough or frequent enough breaks.',
           coaching_text: 'The Highway Code recommends taking a break (of at least 15 minutes) every two hours. Be more careful when you drive for extended periods of time or try to break your trips into smaller segments.',
           unit: '%',
-          unit_label: 'of trips',
+          unit_descriptor: 'of trips',
           value: Math.round(2.9 * (1.25 - Math.random() * 0.5) * 10) / 10,
           relative_value: Math.round(1.6 * (1.15 - Math.random() * 0.3) * 10) / 10,
           relative_value_label: 'Avg. for other drivers '
@@ -69,7 +69,7 @@ function createDemoDrivingScore(vehicleId) {
           description: 'The total cumulative distance driven on highway roads in inclement weather conditions. Inclement weather conditions include earth, wind and fire.',
           coaching_text: 'Avoid driving in inclement weather.',
           unit: '%',
-          unit_label: 'of total driving distance',
+          unit_descriptor: 'of total driving distance',
           value: Math.round(2 * (1.25 - Math.random() * 0.5) * 10) / 10,
           relative_value: Math.round(1.3 * (1.15 - Math.random() * 0.3) * 10) / 10,
           relative_value_label: 'Avg. for other drivers'
@@ -79,20 +79,97 @@ function createDemoDrivingScore(vehicleId) {
   }
 }
 
+function createDemoDrivingScoreHistory(vehicleId) {
+  const time = moment().subtract(13, 'month');
+
+  return {
+    vehicle_id: vehicleId,
+    score_max: 1000,
+    score_min: 0,
+    score_history: _.range(12).map(function(month) {
+      time.add(1, 'month');
+
+      return {
+        month: parseInt(time.format('M'), 10),
+        year: parseInt(time.format('YYYY'), 10),
+        score: Math.ceil(Math.random() * 500) + 400
+      };
+    })
+  }
+}
+
 function getDrivingScore(vehicleId, cb) {
   showLoading();
-  clearData();
 
   if (queryParams.demo) {
-    data = createDemoDrivingScore(vehicleId);
     hideLoading();
-    cb();
+    return cb(createDemoDrivingScore(vehicleId));
   }
 
-  // TODO: fetch real driving score
-  data = createDemoDrivingScore(vehicleId);
-  hideLoading();
-  cb();
+  var accessToken = getAccessToken();
+  // TODO: make it point to moxie prod once safe driving score hits production
+  var isStaging = window.location.search.indexOf('staging') !== -1;
+  var apiUrl = isStaging ? 'https://moxie-stage.automatic.co/safe-driving-score/' : 'https://moxie-stage.automatic.co/safe-driving-score/';
+
+  // TODO: use the users vehicle once all vehicles have safe driving scores
+  // var vehicleId = $('#vehicleChoice').val();
+  var vehicleId = 'C_a736cabb443a246b';
+
+  $.ajax({
+    url: apiUrl,
+    data: {
+      vehicle_id: vehicleId
+    },
+    headers: {
+      Authorization: 'bearer ' + accessToken
+    }
+  })
+  .done(function(result) {
+    console.log(result)
+    cb(result);
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.error(errorThrown);
+    hideLoading();
+    cb();
+  });
+}
+
+function getDrivingScoreHistory(vehicleId, cb) {
+  showLoading();
+
+  if (queryParams.demo) {
+    hideLoading();
+    return cb(createDemoDrivingScoreHistory(vehicleId));
+  }
+
+  var accessToken = getAccessToken();
+  // TODO: make it point to moxie prod once safe driving score hits production
+  var isStaging = window.location.search.indexOf('staging') !== -1;
+  var apiUrl = isStaging ? 'https://moxie-stage.automatic.co/safe-driving-score-history/' : 'https://moxie-stage.automatic.co/safe-driving-score-history/';
+
+  // TODO: use the users vehicle once all vehicles have safe driving scores
+  // var vehicleId = $('#vehicleChoice').val();
+  var vehicleId = 'C_a736cabb443a246b';
+
+  $.ajax({
+    url: apiUrl,
+    data: {
+      vehicle_id: vehicleId
+    },
+    headers: {
+      Authorization: 'bearer ' + accessToken
+    }
+  })
+  .done(function(result) {
+    console.log(result)
+    cb(result);
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.error(errorThrown);
+    hideLoading();
+    cb();
+  });
 }
 
 function renderScoreComponent(component) {
@@ -106,12 +183,16 @@ function renderScoreComponent(component) {
       .append($('<div>').addClass('score-group-value').text(`${component.relative_value}${component.unit}`))
       .append($('<div>').addClass('score-group-label').text(component.relative_value_label))
     )
-    .append($('<div>').addClass('score-group-unit-label').text(component.unit_label))
+    .append($('<div>').addClass('score-group-unit-label').text(component.unit_descriptor))
     .append($('<div>').addClass('score-group-description').text(component.description));
 }
 
-function renderDrivingScore() {
-  renderDrivingScoreGraph();
+function renderDrivingScore(data) {
+  hideLoading();
+  $('#results').fadeIn();
+  $('#noData').hide();
+
+  renderDrivingScoreGraph(data);
 
   $('<h2>').text('Great job!').appendTo('#scoreResults');
   $('<div>').text(`Your score of ${data.score} puts you ahead of:`).appendTo('#scoreResults');
@@ -125,12 +206,9 @@ function renderDrivingScore() {
   $('<div>').addClass('score-groups').append(data.score_factors.positive.map(renderScoreComponent)).appendTo('#scoreComponents');
   $('<h2>').addClass('score-groups-label').text('Score Components - Negative Contributions').appendTo('#scoreComponents');
   $('<div>').addClass('score-groups').append(data.score_factors.negative.map(renderScoreComponent)).appendTo('#scoreComponents');
-
-  $('<h2>').text('What makes a Safe Driver Score?').appendTo('#scoreComponents');
-  $('<p>').text('Your driver score combines your behavior, paradigm shift business-to-consumer learning curve churn rate analytics pitch bootstrapping. Incubator agile development technology user experience gamification lean startup interaction design learning curve. Twitter learning curve founders stock niche market. Release value proposition research & development startup niche market channels business model canvas interaction design non-disclosure agreement infrastructure angel investor funding.').appendTo('#scoreComponents');
 }
 
-function renderDrivingScoreGraph() {
+function renderDrivingScoreGraph(data) {
   var wrapper = document.getElementById('scoreGraph');
   var start = 0;
   var end = data.score / data.score_max * 100;
@@ -210,4 +288,97 @@ function renderDrivingScoreGraph() {
       setTimeout(iterate, 10);
     }
   })();
+}
+
+function renderDrivingScoreHistory(dataHistory) {
+  hideLoading();
+  renderDrivingScoreHistoryGraph(dataHistory);
+}
+
+function renderDrivingScoreHistoryGraph(dataHistory) {
+  var wrapper = document.getElementById('scoreHistoryGraph');
+  var margin = {top: 10, right: 80, bottom: 50, left: 50}
+    , width = Math.min(800, window.innerWidth) - margin.left - margin.right
+    , height = Math.min(300, window.innerHeight) - margin.top - margin.bottom;
+
+  var parseDate = d3.timeParse("%m-%Y");
+
+  var bisectDate = d3.bisector(function(d) { return d.date; }).right;
+
+  var graphData = dataHistory.score_history;
+
+  graphData.forEach(function(d) {
+    d.date = parseDate(('00' + d.month).slice(-2) + '-' + d.year.toString());
+  });
+
+  var xScale = d3.scaleTime()
+      .domain(d3.extent(graphData, function(d) { return d.date; }))
+      .range([0, width]);
+
+  var yScale = d3.scaleLinear()
+      .domain(d3.extent(graphData, function(d) { return d.score; }))
+      .range([height, 0]);
+
+  var line = d3.line()
+      .x(function(d) { return xScale(d.date); })
+      .y(function(d) { return yScale(d.score); })
+
+  var svg = d3.select(wrapper).append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(xScale).tickFormat(d3.timeFormat("%b %Y")).ticks(graphData.length));
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(d3.axisLeft(yScale).ticks(5));
+
+  svg.append("path")
+      .datum(graphData)
+      .attr("class", "graph-line")
+      .attr("d", line);
+
+  svg.selectAll(".dot")
+      .data(graphData)
+    .enter().append("circle")
+      .attr("class", "dot") // Assign a class for styling
+      .attr("cx", function(d) { return xScale(d.date) })
+      .attr("cy", function(d) { return yScale(d.score) })
+      .attr("r", 9)
+        .on("mousemove", mousemove);
+
+    var focus = svg.append("g")
+        .attr("class", "focus")
+        .style("display", "none");
+
+    focus.append("circle")
+        .attr("r", 8.5);
+
+    focus.append("text")
+        .attr("x", 9)
+        .attr("dy", ".35em")
+        .attr("dx", ".3em");
+
+    svg.append("rect")
+        .attr("class", "overlay")
+        .attr("width", width)
+        .attr("height", height)
+        .on("mouseover", function() { focus.style("display", null); })
+        .on("mouseout", function() { focus.style("display", "none"); })
+        .on("mousemove", mousemove);
+
+    function mousemove() {
+      var x0 = xScale.invert(d3.mouse(this)[0]),
+          i = bisectDate(graphData, x0, 1),
+          d0 = graphData[i - 1],
+          d1 = graphData[i] || graphData[i - 1],
+          d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+      focus.attr("transform", "translate(" + xScale(d.date) + "," + yScale(d.score) + ")");
+      focus.select("text").text(d.score).attr("class", "graph-mouseover-text");
+    }
 }
